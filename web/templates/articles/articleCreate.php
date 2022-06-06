54c6b54a765195ff2a5296c2f36a428cc8d2445a
yuya.miyazaki <cheeeese4869@gmail.com>
2022-06-06 14:22:45 +0900

    add div

--- web/templates/articles/articleCreate.php
+++ web/templates/articles/articleCreate.php
@@ -14,17 +14,26 @@
 <?php endif; ?>
 <form action="/articles/create" method="post" enctype="multipart/form-data" name="postForm">
     <input type="hidden" name="csrf_token" value="<?= htmlspecialchars($csrf_token, ENT_QUOTES, "UTF-8") ?>">
-    <label class="title" for="title">タイトル</label>
-    <input id="title" type="text" name="title" required>
-    <label class="body" for="body">本文</label>
-    <textarea rows="4" id="body" name="body" required></textarea>
-    <?php foreach( $tags as $tag ): ?>
-        <input type="checkbox" name="tags[]" id="<?= htmlspecialchars($tag['id'], ENT_QUOTES, "UTF-8") ?>" value="<?= htmlspecialchars($tag['id'], ENT_QUOTES, "UTF-8") ?>">
-            <label for="<?= htmlspecialchars($tag['id'], ENT_QUOTES, "UTF-8") ?>"><?= htmlspecialchars($tag['name'], ENT_QUOTES, "UTF-8") ?></label>
-        </input>
-    <?php endforeach; ?>
-    <input type="file" id="images" name="upload_image[]" multiple>
-    <div id="preview"></div>
+    <div>
+        <label class="title" for="title">タイトル</label>
+        <input id="title" type="text" name="title" required>
+    </div>
+
+    <div>
+        <label class="body" for="body">本文</label>
+        <textarea rows="4" id="body" name="body" required></textarea>
+    </div>
+    <div>
+        <?php foreach( $tags as $tag ): ?>
+            <input type="checkbox" name="tags[]" id="<?= htmlspecialchars($tag['id'], ENT_QUOTES, "UTF-8") ?>" value="<?= htmlspecialchars($tag['id'], ENT_QUOTES, "UTF-8") ?>">
+                <label for="<?= htmlspecialchars($tag['id'], ENT_QUOTES, "UTF-8") ?>"><?= htmlspecialchars($tag['name'], ENT_QUOTES, "UTF-8") ?></label>
+            </input>
+        <?php endforeach; ?>
+    </div>
+    <div>
+        <input type="file" id="images" name="upload_image[]" multiple>
+        <div id="preview"></div>
+    </div>
     <input type="submit" name="submit" >
 </form>
 <script>
