721c284bb53ceccdeb6c12783c58858f4958bed3
yuya.miyazaki <cheeeese4869@gmail.com>
2022-06-06 17:02:23 +0900

    commit漏れ

--- web/templates/articles/articleDetail.php
+++ web/templates/articles/articleDetail.php
@@ -5,20 +5,20 @@
     <title>記事</title>
 </head>
 <body>
-    <h3><?= htmlspecialchars($id, ENT_QUOTES, "UTF-8") ?></h3>
-    <?php foreach($images as $image) : ?>
+    <h3><?= htmlspecialchars($article->id, ENT_QUOTES, "UTF-8") ?></h3>
+    <?php foreach($article->images as $image) : ?>
         <img src="../../templates/images/<?= htmlspecialchars($image, ENT_QUOTES, "UTF-8") ?>.png" alt="" style="width:200px; height:200px">
     <?php endforeach;?>
-    <h3><?= htmlspecialchars($title, ENT_QUOTES, "UTF-8") ?></h3>
-    <h3><?= htmlspecialchars($body, ENT_QUOTES, "UTF-8") ?></h3>
+    <h3><?= htmlspecialchars($article->title, ENT_QUOTES, "UTF-8") ?></h3>
+    <h3><?= htmlspecialchars($article->body, ENT_QUOTES, "UTF-8") ?></h3>
     <p>登録しているタグ</p>
     <ul>
-        <?php foreach($tags as $tag) : ?>
+        <?php foreach($article->tags as $tag) : ?>
             <li><?= htmlspecialchars($tag, ENT_QUOTES, "UTF-8") ?></li>
         <?php endforeach;?>
     </ul>
-    <a href="/articles/<?= htmlspecialchars($article[0]['id'], ENT_QUOTES, 'UTF-8') ?>/update">Edit</a>
-    <form action="/articles/<?= htmlspecialchars($article[0]['id'], ENT_QUOTES, 'UTF-8') ?>/delete" method="post">
+    <a href="/articles/<?= htmlspecialchars($article->id, ENT_QUOTES, 'UTF-8') ?>/update">Edit</a>
+    <form action="/articles/<?= htmlspecialchars($article->id, ENT_QUOTES, 'UTF-8') ?>/delete" method="post">
         <input type="hidden" name="csrf_token" value="<?= htmlspecialchars($csrf_token, ENT_QUOTES, "UTF-8") ?>">
         <button type="submit">Delete</button>
     </form>
