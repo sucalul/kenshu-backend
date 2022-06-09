c7df751054aa0c2e730b7bf42726f61d0824f743
yuya.miyazaki <cheeeese4869@gmail.com>
2022-06-09 18:16:15 +0900

    rm todos

--- web/helpers/ThumbnailHelper.php
+++ web/helpers/ThumbnailHelper.php
@@ -1,23 +1,26 @@
 <?php
 
-class ThumbnailHelper {
-    static function checkThumbnail(string $thumbnail_resource) :array {
-        // TODO
-        // $_FILESや$_POSTをこういったところで扱わず、引数に入れて渡して上げる
-        for ($i = 0; $i < count($_FILES['upload_image']['name']); $i++) {
+class ThumbnailHelper
+{
+    static function checkThumbnail(array $post, array $files): array
+    {
+        $resources = array();
+        $thumbnail_resource = '';
+
+        for ($i = 0; $i < count($files['upload_image']['name']); $i++) {
             // file名をuniqueにする
             $resource = uniqid();
             $resources[] = $resource;
-            //サムネイル登録されているファイル名とforで回しているファイル名が一致したらサムネイルとして登録処理する
-            if (isset($_POST['is-thumbnail']) && $_POST['is-thumbnail'] == $_FILES['upload_image']['name'][$i]) {
+            // サムネイル登録されているファイル名とforで回しているファイル名が一致したらサムネイルとして登録処理する
+            if (isset($post['is-thumbnail']) && $post['is-thumbnail'] == $files['upload_image']['name'][$i]) {
                 $thumbnail_resource = $resource;
                 $index = array_search($thumbnail_resource, $resources);
                 array_splice($resources, $index, 1);
             }
             // upload先指定
-            $uploaded_path = 'templates/images/'.$resource.'.png';
+            $uploaded_path = 'templates/images/' . $resource . '.png';
             // fileの移動
-            move_uploaded_file($_FILES['upload_image']['tmp_name'][$i], $uploaded_path);
+            move_uploaded_file($files['upload_image']['tmp_name'][$i], $uploaded_path);
         }
         // サムネイルが登録されていなければ一つ目の画像をサムネイルとする
         if (empty($thumbnail_resource)) {
